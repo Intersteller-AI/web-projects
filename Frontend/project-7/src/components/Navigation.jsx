78150ffbc4d4a83a71726edac3a46fea9982ec5c
Priyanshu Sahu <95582561+Intersteller-AI@users.noreply.github.com>
2023-03-19 12:52:49 -0700

    Update Navigation.jsx

--- Frontend/project-7/src/components/Navigation.jsx
+++ Frontend/project-7/src/components/Navigation.jsx
@@ -1,11 +1,28 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { BiHomeAlt2, BiCategory } from "react-icons/bi";
 import { TbBrandYoutubeKids, TbCirclePlus } from "react-icons/tb";
 import { MdOutlineSubscriptions } from "react-icons/md";
 import { AiOutlineSetting } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { categoryActions } from "../redux/store";
+import { categories } from "../constants";
 
 const Navigation = () => {
-  const mobileFooter = (() => {
+  const catSelector = useSelector((state) => state.category);
+  const dispath = useDispatch();
+
+  const setHidden = () => {
+    console.log(document.body.style.overflow);
+    if (document.body.style.overflow !== "hidden") {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "scroll";
+    }
+  };
+
+  const [lander, setLander] = useState(false);
+
+  const mobileFooter = () => {
     var d = document,
       i18n = { current: "current" },
       navs = d.querySelectorAll(".mobile-footer");
@@ -56,38 +73,89 @@ const Navigation = () => {
       l.style.width = a.offsetWidth + "px";
       l.style.left = a.offsetLeft + a.offsetWidth / 2 + "px";
     }
-  });
-  
+  };
+
   useEffect(() => {
     mobileFooter();
-  }, [mobileFooter]);
-
+  }, [dispath]);
 
   return (
     <div className="navigation">
+      <div
+        style={{
+          opacity: !lander ? "0" : "1",
+          pointerEvents: !lander ? "none" : "initial",
+          transition: "all 0.4s ease-in-out",
+        }}
+        className="absolute h-72 w-[95vw] drop-shadow-lg special-grad left-[50%] translate-x-[-50%] bottom-[110%] rounded-lg overflow-y-scroll hide-scrollbar"
+      >
+        <div className="w-full flex flex-wrap p-4 z-50">
+          {categories.map((val, index) => (
+            <div
+              onClick={() => {
+                setLander(false);
+                dispath(categoryActions.selectCategory(val.name));
+              }}
+              className="px-3 mx-1 my-2 py-1 drop-shadow-xl bg-darkBlue rounded-md"
+              key={`nav_${val}_${index}`}
+            >
+              <h1 className="font-semibold text-[5vw]">{val.name}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
       <nav className="mobile-footer" role="menulist">
         <a
-          href="#!"
+          href="#home"
           role="menuitem"
           className="active"
           aria-describedby="nav-current"
+          onClick={() => {
+            setLander(false);
+            dispath(categoryActions.selectCategory("New Videos"));
+          }}
         >
           <BiHomeAlt2 className="" />
           Home
         </a>
-        <a href="#!" role="menuitem">
+        <a
+          onClick={() => {
+            setLander(true);
+          }}
+          className=""
+          href="#categories"
+          role="menuitem"
+        >
           <BiCategory className="" />
           Categories
         </a>
-        <a href="#!" role="menuitem">
+        <a
+          onClick={() => {
+            setLander(false);
+          }}
+          href="#add"
+          role="menuitem"
+        >
           <TbCirclePlus className="" />
           Add
         </a>
-        <a href="#!" role="menuitem">
+        <a
+          onClick={() => {
+            setLander(false);
+          }}
+          href="#subscription"
+          role="menuitem"
+        >
           <MdOutlineSubscriptions className="" />
           Subscriptions
         </a>
-        <a href="#!" role="menuitem">
+        <a
+          onClick={() => {
+            setLander(false);
+          }}
+          href="#settings"
+          role="menuitem"
+        >
           <AiOutlineSetting className="" />
           Settings
         </a>
