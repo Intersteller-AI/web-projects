f7abe07388bcad41fbf8111c7a95f0050e22ffa3
Priyanshu Sahu <95582561+Intersteller-AI@users.noreply.github.com>
2023-03-19 23:04:31 -0700

    Update ChannelDetail.jsx

--- Frontend/project-7/src/pages/ChannelDetail.jsx
+++ Frontend/project-7/src/pages/ChannelDetail.jsx
@@ -114,7 +114,7 @@ const ChannelDetail = () => {
                 : "video"
             }`}</h4>
           </div>
-          <h3 className="text-[16px] ss:mt-4 mt-2 max-w-[680px] text-slate-400">
+          <h3 className="text-[16px] overflow-hidden ss:mt-4 mt-2 max-w-[680px] text-slate-400">
             <SmartText text={ChannelDetail?.snippet.description} />
           </h3>
         </div>
