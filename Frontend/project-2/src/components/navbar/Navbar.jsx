7d021306a4dc6d92dc245e0294c11d6f8fe243c5
Priyanshu Sahu <95582561+Intersteller-AI@users.noreply.github.com>
2023-04-13 11:11:25 +0530

    Update Navbar.jsx

--- Frontend/project-2/src/components/navbar/Navbar.jsx
+++ Frontend/project-2/src/components/navbar/Navbar.jsx
@@ -27,8 +27,7 @@ const Navbar = () => {
         </div>
       </div>
       <div className="gpt3__navbar-sign">
-        <p>Sign in</p>
-        <button type="button">Sign up</button>
+        <p>Sign in</p>         <button type="button">Sign up</button>
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu
